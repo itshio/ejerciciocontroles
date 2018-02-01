ffd4b76191b55483c1ee1decd425bcefbd616c65
darkclif <morpheoosmatrix@gmail.com>
2018-02-01 13:43:44 +0100

    controles

--- app/src/main/java/com/example/ejerciciocontroles/MainActivity.java
+++ app/src/main/java/com/example/ejerciciocontroles/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.ejerciciocontroles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox cbsopa, cbensalada, cbnachos, cbcarne, cbpescado;
     RadioGroup rgbebida;
+    Spinner spbebidas;
 
 
     @Override
@@ -25,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
         cbcarne = (CheckBox)findViewById(R.id.cbcarne);
         cbpescado = (CheckBox)findViewById(R.id.cbpescado);
         rgbebida =(RadioGroup)findViewById(R.id.radiogroup_sino);
+        spbebidas =(Spinner)findViewById(R.id.spbebidas);
+
+
+        String [] bebidas = {"Cerveza","Vino","Fanta","Coca cola","Seven up"};
+
+        ArrayAdapter<String>adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1,bebidas);
+
+        spbebidas.setAdapter(adaptador);
+
+
+
+
 
 
 
@@ -62,17 +78,25 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), "has elegido "+comida, Toast.LENGTH_SHORT).show();
 
+
+        //EVENTO RADIOBUTTOM
+
+
         int id=rgbebida.getCheckedRadioButtonId();
 
         RadioButton rbseleccionado=(RadioButton)findViewById(id);
         String textoalpulsar = rbseleccionado.getText().toString();
         Toast.makeText(getApplicationContext(), "Bebida: "+textoalpulsar, Toast.LENGTH_SHORT).show();
 
-
-
+        //EVENTO SPINNER
+        String bebidaseleccionada= spbebidas.getSelectedItem().toString();
+        Toast.makeText(getApplicationContext(), "Usted desea "+bebidaseleccionada, Toast.LENGTH_SHORT).show();
 
 
     }
+
+
+
 
 
 }//NO BORRAR
